da7cefef3671484530460e0ca9db04dc0c9b0951
Eusi Bradford-Cuff <thepopcornfiend24@gmail.com>
2026-06-17 16:39:51 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,9 @@
+full_name = "Eusi Bradford-Cuff"
+nickname = "Eusiah"
+age = 17
+used_python_before = False
+hobbies = ["piano", "video games", "hiking" ]
+favorites = {"color": "blue", "game": "Persona 4", "music genre": "jazz"}
+favorite_jazz_musicians = {"pianist": "Herbie Hancock", "drummer": "Art Blakey", "trumpeter": "Miles Davis", "saxophonist": "Kamasi Washington", "bassist": "Jaco Pastorius"}
+
+print(full_name, nickname, age, used_python_before, hobbies, favorites, favorite_jazz_musicians)
